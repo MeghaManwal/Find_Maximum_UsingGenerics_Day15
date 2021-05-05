7b9f70cd281725fe97a755a2d1321447e6c591a3
MeghaManwal <https://github.com/MeghaManwal/Basic-Calculator.git>
2021-05-05 10:28:05 +0530

    Added:To Find Maximum among 3 integers and write TestCases

---
+++ src/main/java/com/Generics/FindMaximum.java
@@ -0,0 +1,15 @@
+package com.Generics;
+
+public class FindMaximum {
+	
+	public static int getMax_IntegerValue(int[] numbers){
+         int maxValue = numbers[0];
+         for(int i=1;i < numbers.length;i++){
+		   if(numbers[i] > maxValue){
+              maxValue = numbers[i];
+		   }
+		 }
+		 return maxValue;
+	}
+
+}
